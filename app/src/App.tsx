57877af2e4fcb53e22ce9883ec8ec171be0d686b
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-02-04 16:10:10 -0500

    Set up first part of the ray tracer.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,37 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const imageWidth = 256;
+  const imageHeight = 256;
+
+  const drawImage = (ctx: CanvasRenderingContext2D) => {
+    for (let i = 0; i < imageWidth; i++) {
+      for (let j = 0; j < imageHeight; j++) {
+        let r = i / imageWidth;
+        let g = j / imageHeight;
+        let b = 0.25;
+
+        let ir = 255.0 * r;
+        let ig = 255.0 * g;
+        let ib = 255.0 * b;
+
+        ctx.fillStyle = `rgba(${ir}, ${ig}, ${ib}, 1.0)`;
+        console.log(ctx.fillStyle);
+        ctx.fillRect(i, j, 1, 1);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+    drawImage(ctx);
+  }, []);
+
   return (
     <div className="App">
-      <h1>Vite + React</h1>
+      <canvas id="myCanvas" width={256} height={256}></canvas>
     </div>
   );
 }
