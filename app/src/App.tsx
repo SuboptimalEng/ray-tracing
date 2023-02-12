67ce3de5821eefcc44fe6c16a2e9630083eda507
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-02-12 11:47:02 -0500

    Fix skybox for raytracer.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,11 +2,22 @@ import { useEffect, useState } from "react";
 import { Ray } from "./packages/Ray";
 import { Vec3, vadd, vsub, vscale } from "./packages/Vec3";
 
+const rayColor = (r: Ray): Vec3 => {
+  const unitDirection = r.unitVector();
+  const t = 0.5 * (unitDirection.y + 1.0);
+  const white = new Vec3(1.0, 1.0, 1.0);
+  const black = new Vec3(0.0, 0.0, 0.0);
+  const skyBlue = new Vec3(0.5, 0.7, 1.0);
+  // return vscale(vadd(vscale(white, 1.0 - t), vscale(black, t)), 255);
+  return vscale(vadd(vscale(white, 1.0 - t), vscale(skyBlue, t)), 255);
+};
+
 function App() {
   // canvas
   const aspectRatio = 16.0 / 9.0;
   const canvasWidth = 400;
   const canvasHeight = Math.floor(canvasWidth / aspectRatio);
+  const pixelSize = 10;
 
   // camera
   const viewportHeight = 2.0;
@@ -17,26 +28,14 @@ function App() {
   const horizontal = new Vec3(viewportWidth, 0.0, 0.0);
   const vertical = new Vec3(0.0, viewportHeight, 0.0);
 
-  const scaledHorizontal = vscale(horizontal, 0.5);
-  const scaledVertical = vscale(vertical, 0.5);
-
-  let lowerLeftCorner = vsub(origin, scaledHorizontal);
-  lowerLeftCorner = vsub(lowerLeftCorner, scaledVertical);
+  let lowerLeftCorner = origin;
+  lowerLeftCorner = vsub(lowerLeftCorner, vscale(horizontal, 0.5));
+  lowerLeftCorner = vsub(lowerLeftCorner, vscale(vertical, 0.5));
   lowerLeftCorner = vsub(lowerLeftCorner, new Vec3(0.0, 0.0, focalLength));
 
-  const rayColor = (r: Ray): Vec3 => {
-    const unitDirection = r.unitVector();
-    const t = 0.5 * (unitDirection.y + 1.0);
-    const white = new Vec3(1.0, 1.0, 1.0);
-    const black = new Vec3(0.0, 0.0, 0.0);
-    const skyBlue = new Vec3(0.5, 0.7, 1.0);
-    // return vscale(vadd(vscale(white, 1.0 - t), vscale(black, t)), 255);
-    return vscale(vadd(vscale(white, 1.0 - t), vscale(skyBlue, t)), 255);
-  };
-
   const drawImage = (ctx: CanvasRenderingContext2D) => {
-    for (let j = canvasHeight - 1; j > 0; j--) {
-      for (let i = 0; i < canvasWidth; i++) {
+    for (let j = canvasHeight - 1; j > 0; j -= pixelSize) {
+      for (let i = 0; i < canvasWidth; i += pixelSize) {
         const u = i / (canvasWidth - 1);
         const v = j / (canvasHeight - 1);
 
@@ -47,11 +46,10 @@ function App() {
         rayDirection = vsub(rayDirection, origin);
         const r: Ray = new Ray(origin, rayDirection);
 
-        // debugger;
-
         const color = rayColor(r);
         ctx.fillStyle = `rgba(${color.x}, ${color.y}, ${color.z}, 1)`;
-        ctx.fillRect(i, j, 1, 1);
+        // note: paints down as j increases in height, this is done for ease of use
+        ctx.fillRect(i, canvasHeight - j, pixelSize, pixelSize);
 
         // let r = i / canvasWidth;
         // let g = j / canvasHeight;
