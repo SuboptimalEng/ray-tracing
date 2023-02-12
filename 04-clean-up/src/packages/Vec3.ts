2d2c4173247522e1aa4f267222ca9560f3973a77
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-02-12 17:33:23 -0500

    Create new folder to clean up code.

---
+++ 04-clean-up/src/packages/Vec3.ts
@@ -0,0 +1,72 @@
+class Vec3 {
+  x: number;
+  y: number;
+  z: number;
+
+  constructor(x = 0, y = 0, z = 0) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  getNew() {
+    return new Vec3(this.x, this.y, this.z);
+  }
+
+  add(v: Vec3): Vec3 {
+    this.x += v.x;
+    this.y += v.y;
+    this.z += v.z;
+    return new Vec3(this.x, this.y, this.z);
+  }
+
+  subtract(v: Vec3): void {
+    this.x -= v.x;
+    this.y -= v.y;
+    this.z -= v.z;
+  }
+
+  multiply(v: Vec3): void {
+    this.x *= v.x;
+    this.y *= v.y;
+    this.z *= v.z;
+  }
+
+  scale(n: number): Vec3 {
+    this.x *= n;
+    this.y *= n;
+    this.z *= n;
+    return new Vec3(this.x, this.y, this.z);
+  }
+
+  lengthSquared(): number {
+    return this.x * this.x + this.y * this.y + this.z * this.z;
+  }
+
+  length(): number {
+    return Math.sqrt(this.lengthSquared());
+  }
+}
+
+const dot = (a: Vec3, b: Vec3): number => {
+  return a.x * b.x + a.y * b.y + a.z * b.z;
+};
+
+const vadd = (a: Vec3, b: Vec3): Vec3 => {
+  return new Vec3(a.x + b.x, a.y + b.y, a.z + b.z);
+};
+
+const vsub = (a: Vec3, b: Vec3): Vec3 => {
+  return new Vec3(a.x - b.x, a.y - b.y, a.z - b.z);
+};
+
+const vscale = (a: Vec3, n: number): Vec3 => {
+  return new Vec3(a.x * n, a.y * n, a.z * n);
+};
+
+const unitVector = (a: Vec3): Vec3 => {
+  const vectorLength = a.length();
+  return new Vec3(a.x / vectorLength, a.y / vectorLength, a.z / vectorLength);
+};
+
+export { Vec3, dot, vadd, vsub, vscale, unitVector };
