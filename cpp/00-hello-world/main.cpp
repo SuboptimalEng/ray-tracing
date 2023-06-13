1ce53d249ca324da1e66eb244fde95ac1de0b691
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-06-12 22:32:53 -0400

    Get official code working.

--- cpp/00-hello-world/main.cpp
+++ cpp/00-hello-world/main.cpp
@@ -7,10 +7,8 @@ using namespace std;
 int main() {
   const int image_width = 256;
   const int image_height = 256;
-  // int image_width = 256;
-  // int image_height = 256;
 
-  // std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+  std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
   for (int j = image_height - 1; j >= 0; j--) {
     for (int i = 0; i < image_width; i++) {
