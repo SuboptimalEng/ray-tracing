e95de942eac91ab053f64eaa3f30c6778bc867b9
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-06-18 15:19:22 -0400

    Shade sphere with surface normals.

--- cpp/02-sphere/main.cpp
+++ cpp/02-sphere/main.cpp
@@ -8,23 +8,45 @@
 
 using namespace std;
 
-bool hit_sphere(const Point3& center, double radius, const Ray& r) {
+// notes on the sphere equation
+// equation of sphere
+// x^2 + y^2 + z^2 = r^2
+
+// equation of sphere at center C
+// (x - Cx)^2 + (y - Cy)^2 + (z - Cz)^2 = r^2
+
+// vector from center C = (Cx, Cy, Cz) to point P = (x, y, z)
+// dot((P - C), (P - C)) = (x - Cx)^2 + (y - Cy)^2 + (z - Cz)^2
+// this can be thought of as the radius of the sphere
+// dot((P - C), (P - C)) = r^2
+
+// in our case, P(t) is the ray with origin A, and direction b
+// dot((P(t) - C), (P(t) - C)) = r^2
+// dot((A + tb - C), (A + tb - C)) = r^2
+// simplifing this equation gives us this hit_sphere method
+double hit_sphere(const Point3& center, double radius, const Ray& r) {
   Vec3 oc = r.origin() - center;
   auto a = dot(r.direction(), r.direction());
   auto b = 2.0 * dot(oc, r.direction());
   auto c = dot(oc, oc) - radius * radius;
   auto discriminant = b * b - 4 * a * c;
-  return (discriminant > 0);
+  if (discriminant < 0) {
+    return -1.0;
+  } else {
+    return (-b - sqrt(discriminant)) / (2.0 * a);
+  }
 }
 
 // linearly blends white and blue depending on the y coords
 // after scaling it from -1 <= y <= 1
 Color ray_color(const Ray& r) {
-  if (hit_sphere(Point3(0, 0, -1), 0.5, r)) {
-    return Color(1, 0, 0);
+  auto t = hit_sphere(Point3(0, 0, -1), 0.5, r);
+  if (t > 0.0) {
+    Vec3 N = unit_vector(r.at(t) - Vec3(0, 0, -1));
+    return 0.5 * Color(N.x() + 1, N.y() + 1, N.z() + 1);
   }
   Vec3 unit_direction = unit_vector(r.direction());
-  auto t = 0.5 * (unit_direction.y() + 1.0);
+  t = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
 }
 
