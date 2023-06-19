59e5d0a677b4f39b14b0923a2b105ad3f8f5fc58
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-06-19 11:43:11 -0400

    Set up folder for depth of field (aka defocus blur).

---
+++ cpp/08-positionable-camera/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "camera.h"
+#include "color.h"
+#include "hittable_list.h"
+#include "material.h"
+#include "rtweekend.h"
+#include "sphere.h"
+
+// linearly blends white and blue depending on the y coords
+// after scaling it from -1 <= y <= 1
+Color ray_color(const Ray& r, const Hittable& world, int depth) {
+  hit_record rec;
+
+  if (depth <= 0) {
+    return Color(0, 0, 0);
+  }
+
+  if (world.hit(r, 0.001, infinity, rec)) {
+    // Point3 target = rec.p + rec.normal + random_in_unit_sphere();
+    // Point3 target = rec.p + rec.normal + random_unit_vector();
+
+    // Point3 target = rec.p + random_in_hemisphere(rec.normal);
+    // return 0.5 * ray_color(Ray(rec.p, target - rec.p), world, depth - 1);
+
+    Ray scattered;
+    Color attenuation;
+    if (rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+      return attenuation * ray_color(scattered, world, depth - 1);
+    }
+    return Color(0, 0, 0);
+  }
+  Vec3 unit_direction = unit_vector(r.direction());
+  auto t = 0.5 * (unit_direction.y() + 1.0);
+  return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
+}
+
+int main() {
+  // image
+  const auto aspect_ratio = 16.0 / 9.0;
+  const int image_width = 800;
+  const int image_height = static_cast<int>(image_width / aspect_ratio);
+  const int samples_per_pixel = 24;
+  const int max_depth = 8;
+
+  // world
+  HittableList world;
+
+  auto material_ground = make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
+  auto material_center = make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
+  auto material_left = make_shared<Dielectric>(1.5);
+  auto material_right = make_shared<Metal>(Color(0.8, 0.6, 0.2), 1.0);
+
+  world.add(make_shared<Sphere>(Point3(0.0, -100.5, -1.0), 100.0, material_ground));
+  world.add(make_shared<Sphere>(Point3(0.0, 0.0, -1.0), 0.5, material_center));
+  world.add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, material_left));
+  world.add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), -0.4, material_left));
+  world.add(make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, material_right));
+
+  // camera
+  Camera cam(Point3(-2, 2, 1), Point3(0, 0, -1), Vec3(0, 1, 0), 20, aspect_ratio);
+
+  std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+  for (int j = image_height - 1; j >= 0; j--) {
+    std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
+    for (int i = 0; i < image_width; i++) {
+      Color pixel_color(0, 0, 0);
+      for (int s = 0; s < samples_per_pixel; s++) {
+        auto u = (i + random_double()) / (image_width - 1);
+        auto v = (j + random_double()) / (image_height - 1);
+        Ray r = cam.get_ray(u, v);
+        pixel_color += ray_color(r, world, max_depth);
+      }
+      write_color(std::cout, pixel_color, samples_per_pixel);
+    }
+  }
+
+  std::cerr << "\nDone.\n";
+
+  return 0;
+}
---
+++ cpp/09-depth-of-field/camera.h
@@ -0,0 +1,35 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "rtweekend.h"
+
+class Camera {
+ public:
+  Camera(Point3 lookfrom, Point3 lookat, Vec3 vup, double vfov, double aspect_ratio) {
+    auto theta = degrees_to_radians(vfov);
+    auto h = tan(theta / 2);
+    auto viewport_height = 2.0 * h;
+    auto viewport_width = aspect_ratio * viewport_height;
+
+    auto w = unit_vector(lookfrom - lookat);
+    auto u = unit_vector(cross(vup, w));
+    auto v = cross(w, u);
+
+    origin = lookfrom;
+    horizontal = viewport_width * u;
+    vertical = viewport_height * v;
+    lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
+  }
+
+  Ray get_ray(double s, double t) const {
+    return Ray(origin, lower_left_corner + s * horizontal + t * vertical - origin);
+  }
+
+ private:
+  Point3 origin;
+  Point3 lower_left_corner;
+  Vec3 horizontal;
+  Vec3 vertical;
+};
+
+#endif
